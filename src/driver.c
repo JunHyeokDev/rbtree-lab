adf102d60e3d34444d9743d575e53ed9d899b020
JunHyeokDev <fixme1537@gmail.com>
2024-04-20 10:50:12 +0000

    delete functionality completed!

--- src/driver.c
+++ src/driver.c
@@ -1,16 +1,35 @@
 #include "rbtree.h"
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-void rbtree_to_print(node_t *t, node_t *nil);
-int main(int argc, char *argv[])
-{
-    rbtree *t = new_rbtree();
-    for (int i = 1; i <= 15; i ++) {
-        rbtree_insert(t, i);
-    }
-    rbtree_to_print(t->root, t->nil);
-    return 0;
-}
+
+// void rbtree_to_print(node_t *t, node_t * nil) {
+//   // TODO: implement to_print
+//   // printf("t.print 진입\n");
+//   if( t == nil ){
+//     return;
+//   }
+//   if (t != nil){
+//     char tmp;
+//     if (t->color == RBTREE_RED) {
+//         tmp = 'R';
+//     } else {
+//         tmp = 'B';
+//     }
+//     printf("My Vale is : %d  ",t->key);
+    
+//     printf("My color is : %c \n",tmp);
+//   }
+//   if(t->left != nil){
+//     rbtree_to_print(t->left, nil);
+//   }
+//   if(t->right != nil){
+//     rbtree_to_print(t->right, nil);
+//   }
+//   // return 0;
+// }
+
 void rbtree_to_print(node_t *t, node_t *nil)
 {
     // TODO: implement to_print
@@ -19,7 +38,38 @@ void rbtree_to_print(node_t *t, node_t *nil)
         printf("노드 바닥이에용\n");
         return;
     }
-    printf("t.print %d\n", t->key);
+    printf("key = %d, color = %d \n", t->key, t->color);
     rbtree_to_print(t->left, nil);
     rbtree_to_print(t->right, nil);
+}
+
+int main(int argc, char *argv[]) {
+    rbtree *t = new_rbtree();
+    // node_t *p = rbtree_insert(t, 26);
+    // key_t entries[] = {10, 5, 8, 34, 67};
+    key_t entries[] = {10, 5, 8, 34, 67, 23, 156, 24, 2, 12, 24, 36, 990, 25};
+    const size_t n = sizeof(entries) / sizeof(entries[0]);
+    for (int i = 0; i < n; i++)
+    {
+        rbtree_insert(t, entries[i]);
+        /* code */
+    }
+    printf("최초 트리 리스트 출력");
+    rbtree_to_print(t->root,t->nil);
+    for (int i = 0; i < n; i++)
+    {
+        node_t *p = rbtree_find(t, entries[i]);
+        // printf("arr[%d] = %d\n", i, arr[i]);
+        // assert(p != NULL);
+        // assert(p->key == arr[i]);
+        rbtree_erase(t, p);
+        // printf("%d 키-", entries[i]);
+        // printf("%d 번째 출력\n", i+1);
+        rbtree_to_print(t->root,t->nil);
+    }
+    // rbtree_insert(t, 17);
+    // rbtree_insert(t, 41);
+    // rbtree_insert(t, 47);
+    // rbtree_insert(t, 21);
+    printf("\n");
 }
--- src/rbtree.c
+++ src/rbtree.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 
 void RB_Insert_Fixup(rbtree *t, node_t *z);
+node_t *Find_successor(node_t *cur, node_t *nil);
+void RB_TransPlant(rbtree *t, node_t *u, node_t *v);
+void RB_Delete_fixup(rbtree *t, node_t *cur);
+rbtree *new_rbtree(void);
+node_t *rbtree_min(const rbtree *t);
 
 rbtree *new_rbtree(void)
 {
@@ -107,7 +112,7 @@ void Left_Rotate(rbtree *t, node_t *cur)
 
 void Right_Rotate(rbtree *t, node_t *cur)
 {
-    printf("라이트 로테이트");
+    printf("라이트 로테이트\n");
     // y = cur
     node_t *target = cur->left;
     cur->left = target->right;
@@ -138,7 +143,7 @@ void Right_Rotate(rbtree *t, node_t *cur)
 
 void RB_Insert_Fixup(rbtree *t, node_t *cur)
 {
-    printf("픽스업 진입");
+    printf("픽스업 진입\n");
     while (cur->parent->color == RBTREE_RED)
     {
         if (cur->parent == cur->parent->parent->left)
@@ -191,26 +196,36 @@ node_t *rbtree_find(const rbtree *t, const key_t key)
 {
     // TODO: implement find
     node_t *cur = t->root;
-    while (cur != t->nil) {
-        if (cur->key < key) 
+
+    while (cur != t->nil)
+    {
+        if (cur->key == key)
+        {
+            return cur;
+        }
+        else if (cur->key < key)
         {
             cur = cur->right;
         }
-        else if (cur->key > key)
+        else
         {
             cur = cur->left;
-        }
-        else {
-            return cur;
         }
     }
     return NULL;
 }
 
-node_t *rbtree_min(const rbtree *t)
-{
-    // TODO: implement find
-    return t->root;
+node_t *rbtree_min(const rbtree *t) {
+  // TODO: implement find
+  if (t->root == t->nil) {
+    return NULL;
+  }
+  node_t * curr = t->root;
+
+  while (curr->left != t->nil) {
+    curr = curr->left;
+  }
+  return curr;
 }
 
 node_t *rbtree_max(const rbtree *t)
@@ -219,10 +234,172 @@ node_t *rbtree_max(const rbtree *t)
     return t->root;
 }
 
-int rbtree_erase(rbtree *t, node_t *p)
+void RB_TransPlant(rbtree *t, node_t *u, node_t *v)
+{
+    // printf("TransPlant 진입 u is : %d  !! \n", u->key );
+    // printf("TransPlant 진입 v is : %d  !! \n", v->key );
+
+    if (u->parent == t->nil)
+    {
+        t->root = v;
+    }
+    else if (u == u->parent->left)
+    {
+        u->parent->left = v;
+    }
+    else
+    {
+        u->parent->right = v;
+    }
+    // printf("TransPlant 탈출 u parent is : %d  !! \n", u->parent->key );
+    // printf("TransPlant 탈출 v parent is : %d  !! \n", v->parent->key );
+
+    v->parent = u->parent;
+    // printf("TransPlant 탈출 u parent is : %d  !! \n", u->parent->key );
+    // printf("TransPlant 탈출 v parent is : %d  !! \n", v->parent->key );
+
+}
+
+node_t *Find_successor(node_t *cur, node_t *nil)
+{
+    printf("Before Successor 값 : %d !! \n",cur->key );
+    while (cur->left != nil)
+    {
+        cur = cur->left;
+    }
+    printf("After Successor 값 : %d !! \n",cur->key );
+    return cur;
+}
+
+int rbtree_erase(rbtree *t, node_t *target)
 {
     // TODO: implement erase
-    return 0;
+    node_t *replcae = target;
+    color_t target_original_color = replcae->color;
+    node_t *successor;
+    node_t *x;
+    // printf("Target 값 : %d !! \n",target->key );
+    // printf("Target right 값 : %d !! \n",target->right->key );
+    // printf("Target left  값 : %d !! \n",target->left->key );
+
+    if (target->left == t->nil)
+    {
+        x = target->right;
+        RB_TransPlant(t, target, target->right);
+    }
+    else if (target->right == t->nil)
+    {
+        x = target->left;
+        RB_TransPlant(t, target, target->left);
+    }
+    else
+    {
+        // printf("Target Before go to Find_Successor값 : %d !! \n",target->key );
+        
+        successor = target->right;
+        while(successor->left != t->nil){
+            successor = successor->left;
+        }
+
+        target_original_color = successor->color;
+        x = successor->right;
+        
+        if (successor->parent == target)
+        {
+            x->parent = successor;
+        }
+        else
+        {
+            RB_TransPlant(t, successor, successor->right);
+            successor->right = target->right;
+            successor->right->parent = successor;
+        }
+        RB_TransPlant(t, target, successor);
+        successor->left = target->left;
+        successor->left->parent = successor;
+        successor->color = target->color;
+    }
+    // printf("target is deleted? or ? %d\n", target->key);
+    if (target_original_color == RBTREE_BLACK)
+    {
+        RB_Delete_fixup(t, x);
+    }
+    // free zone
+    free(target);
+
+    return 0; // return what ?
+}
+
+void RB_Delete_fixup(rbtree *t, node_t *cur)
+{
+    node_t *bro;
+    while (cur != t->root && cur->color == RBTREE_BLACK) {
+        if (cur == cur->parent->left)
+        {
+            bro = cur->parent->right;
+            if (bro->color == RBTREE_RED)
+            {                                    // 형제의 컬러가 Red인 경우
+                bro->color = RBTREE_BLACK;       // 형제는 Black으로
+                cur->parent->color = RBTREE_RED; // 부모의 색상을 RED로 바꾼 뒤 부모를 기준으로 회전.
+                Left_Rotate(t, cur->parent);
+                bro = cur->parent->right;
+            }
+            if (bro->left->color == RBTREE_BLACK && bro->right->color == RBTREE_BLACK)
+            {
+                bro->color = RBTREE_RED; // 여기서 bro가 쭈왑 당해서 레드가 됨.
+                cur = cur->parent;       // cur는 위로 올림. 따라서 cur 는 Doubly black이 되거나 Red_and_black이 되어
+                // 다음 이터레이션에서 또 다른 케이스를 만나게 될 것임.
+            }
+            else
+            {
+                if (bro->right->color == RBTREE_BLACK)
+                {
+                    bro->left->color = RBTREE_BLACK;
+                    bro->color = RBTREE_RED;
+                    Right_Rotate(t, bro);
+                    bro = cur->parent->right;
+                }
+                bro->color = cur->parent->color;
+                cur->parent->color = RBTREE_BLACK;
+                bro->right->color = RBTREE_BLACK;
+                Left_Rotate(t, cur->parent);
+                cur = t->root;
+            }
+        }
+        else
+        {
+            bro = cur->parent->left;
+            if (bro->color == RBTREE_RED)
+            {                                    // 형제의 컬러가 Red인 경우
+                bro->color = RBTREE_BLACK;       // 형제는 Black으로
+                cur->parent->color = RBTREE_RED; // 부모의 색상을 RED로 바꾼 뒤 부모를 기준으로 회전.
+                Right_Rotate(t, cur->parent);
+                bro = cur->parent->left;
+            }
+            if (bro->right->color == RBTREE_BLACK && bro->left->color == RBTREE_BLACK)
+            {
+                bro->color = RBTREE_RED; // 여기서 bro가 쭈왑 당해서 레드가 됨.
+                cur = cur->parent;       // cur는 위로 올림. 따라서 cur 는 Doubly black이 되거나 Red_and_black이 되어
+                // 다음 이터레이션에서 또 다른 케이스를 만나게 될 것임.
+            }
+            else
+            {
+                if (bro->left->color == RBTREE_BLACK)
+                {
+                    bro->right->color = RBTREE_BLACK;
+                    bro->color = RBTREE_RED;
+                    Left_Rotate(t, bro);
+                    bro = cur->parent->left;
+                }
+                bro->color = cur->parent->color;
+                cur->parent->color = RBTREE_BLACK;
+                bro->left->color = RBTREE_BLACK;
+                Right_Rotate(t, cur->parent);
+                cur = t->root;
+            }
+        }
+    }
+    cur->color = RBTREE_BLACK;
 }
 
 int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n)
